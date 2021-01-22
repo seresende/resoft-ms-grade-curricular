f4d674a68f770ec8177e6d4cc440c3e057a39192
Sérgio <sergio.resende@gmail.com>
2021-01-22 11:08:13 -0300

    Inclusão dos fontes

---
+++ src/main/java/com/resoft/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.resoft.cliente.escola.gradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricularApplication.class, args);
+	}
+
+}
